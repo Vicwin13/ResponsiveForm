0eae297c6554e11536a1db099c7444d3ba18b4a3
Vicwin13 <vnwimo13@gmail.com>
2022-06-17 13:21:17 +0100

    finally

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let firstName = document.getElementById("FirstName").value;
-let lastName = document.getElementById("LastName");
+let FirstName = document.getElementById("FirstName");
+let LastName = document.getElementById("LastName");
 let email = document.getElementById("email");
-let  password = document.getElementById("password");
+let password = document.getElementById("password");
+
 let submit = document.getElementById("submit");
-let firstNameError = document.querySelector(".FirstError");
-let Input = document.getElementById("FirstName");
-let icon = document.getElementsByClassName("first");
+let icons = document.getElementById("icon");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
+let icon4 = document.getElementById("icon4");
 
 
 
@@ -14,15 +16,27 @@ submit.addEventListener("click", validate);
 
 function validate(){
 
-   if (firstName.length == 0){
-        firstNameError.innerHTML= "Input First name";
-        firstNameError.style.color = "red";
-        Input.style.border ="1px solid red";
-        return false;
-   } if (!firstName.match(/^[A-Za-z]*&/)){
-    
-        firstNameError.innerHTML = "use the right characters"
-   }
+ console.log("You clicked on the submit button")
+
+icons.style.visibility = "visible";
+FirstName.style.border = " 1.5px solid red"
+FirstName.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
+
+icon2.style.visibility = "visible";
+
+LastName.style.border = " 1.5px solid red"
+LastName.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
+
+icon3.style.visibility = "visible";
+
+email.style.border = " 1.5px solid red"
+email.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
+
+icon4.style.visibility = "visible";
+
+password.style.border = " 1.5px solid red"
+password.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
+
 
 
 };
