e747e7aa5d944b91c340bdd9d9ccfaa76fb83f72
vicwin13 <vnwimo13@gmail.com>
2022-10-18 23:09:28 +0100

    yhh

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 let FirstName = document.getElementById("FirstName");
 let LastName = document.getElementById("LastName");
 let email = document.getElementById("email");
-let password = document.getElementById("password");
 
 let submit = document.getElementById("submit");
 let icons = document.getElementById("icon");
 let icon2 = document.getElementById("icon2");
 let icon3 = document.getElementById("icon3");
-let icon4 = document.getElementById("icon4");
 
+FirstName.addEventListener("keyup", (key) => {
+  let pattern1 = /[\D]$/;
+  let results = pattern1.test(key.target.value);
+  if (!results) {
+    icons.style.visibility = "visible";
+  } else {
+    icons.style.visibility = "hidden";
+  }
+});
 
+LastName.addEventListener("keydown", (key) => {
+  let pattern1 = /[\D]$/;
+  let results = pattern1.test(key.target.value);
+  if (!results) {
+    icon2.style.visibility = "visible";
+  } else {
+    icon2.style.visibility = "hidden";
+  }
+});
 
-submit.addEventListener("click", validate);
-
-
-function validate(){
-
- console.log("You clicked on the submit button")
-
-icons.style.visibility = "visible";
-FirstName.style.border = " 1.5px solid red"
-FirstName.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
-
-icon2.style.visibility = "visible";
-
-LastName.style.border = " 1.5px solid red"
-LastName.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
-
-icon3.style.visibility = "visible";
-
-email.style.border = " 1.5px solid red"
-email.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
-
-icon4.style.visibility = "visible";
-
-password.style.border = " 1.5px solid red"
-password.style.boxShadow = "0px 0px 1px 2px rgba(243, 52, 52, .7)"
-
-
-
-};
+email.addEventListener("keydown", (key) => {
+  let pattern2 = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
+  let results2 = pattern2.test(key.target.value);
+  if (results2) {
+    icon3.style.visibility = "hidden";
+  } else {
+    icon3.style.visibility = "visible";
+  }
+});
